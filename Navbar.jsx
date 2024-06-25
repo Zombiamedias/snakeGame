6a8a0d51ba363f6344e36167b8446399ca902fb0
mapemc <michaellipe@hotmail.com>
2024-06-25 15:45:19 -0500

    snake game version1

---
+++ Navbar.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+
+export function Navbar() {
+    return(
+            <ul className='sidebar'>
+                <li className="linkElement">
+                    <a href='#bgCards'>Proyectos</a>
+                </li>
+                <li className="linkElement">
+                    <a href='#exp' >Experiencia</a>
+                </li>
+                <li className="linkElement">
+                    <a href='#info'>Sobre Mi</a>
+                </li>
+            </ul>
+    )
+  }
